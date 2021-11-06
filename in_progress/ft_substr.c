a293ed3d78f010dcf68af02cfd087eaf4bd59b12
Abdelghani Hammoud <ahammoud@student.42madrid.com>
2021-11-06 22:36:36 +0100

    i made a trash timeout on strtrim

--- in_progress/ft_substr.c
+++ in_progress/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ahammoud <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 16:48:39 by ahammoud          #+#    #+#             */
-/*   Updated: 2021/11/06 17:20:08 by ahammoud         ###   ########.fr       */
+/*   Updated: 2021/11/06 21:23:15 by ahammoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -17,7 +17,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	int		ss;
 
 	ss = ft_strlen((char *)s);
-	if (start > (unsigned int)ss)
+	if (start >= (unsigned int)ss || *s == '\0')
 		return ("\0");
 	str = malloc((len + 1) * sizeof(char));
 	if (!str)
